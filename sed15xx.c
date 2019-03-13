249cf2524b8aadaeb8d9986c0f3697241a01ff1a
Jason Berger <berge472@gmail.com>
2019-03-13 16:02:07 -0400

    updated to fit abstraction layer

--- sed15xx.c
+++ sed15xx.c
@@ -5,10 +5,10 @@
   *@date 03/02/2019
   */
 
+#include "sed15xx.h"
 
-mrt_status_t sed15xx_init(sed15xx_t* dev, mrt_8080_handle_t handle,  int width, int height)
+mrt_status_t sed15xx_init(sed15xx_t* dev, mrt_pbus_handle_t handle,  int width, int height )
 {
-
 
   dev->mHandle = handle;
   dev->mWidth = width;
@@ -18,7 +18,34 @@ mrt_status_t sed15xx_init(sed15xx_t* dev, mrt_8080_handle_t handle,  int width, 
   dev->mBuffer = (uint8_t*)malloc( dev->mBufferSize);
   dev->mInverted = false;
 
+
+/*
+   * Initialize display.  This code follows the sequence in the
+   * example code from the display manufacturer.
+   */
+   sed15xx_cmd( dev , SED15XX_CMD_RESET);
+   sed15xx_cmd( dev , SED15XX_CMD_ADC_SELECT_NORMAL);
+   sed15xx_cmd( dev , SED15XX_CMD_COM_REVERSE);
+   sed15xx_cmd( dev , SED15XX_CMD_LCD_BIAS_1_7);
+   sed15xx_cmd( dev , SED15XX_CMD_POWER_CONTROL(0x7));
+   sed15xx_cmd( dev , SED15XX_CMD_REGULATOR_RESISTOR_RATIO_SET(0x5));
+   sed15xx_cmd( dev , SED15XX_CMD_CONTRAST_SET);
+   sed15xx_cmd( dev , 19);
+   sed15xx_cmd( dev , SED15XX_CMD_START_LINE_SET(0x0));
+   sed15xx_cmd( dev , SED15XX_CMD_BOOSTER_RATIO_SET);
+   sed15xx_cmd( dev , 0x0);
+   sed15xx_cmd( dev , SED15XX_CMD_DISPLAY_ON);
+
   return MRT_STATUS_OK;
+}
+
+void sed15xx_cmd(sed15xx_t* dev, uint8_t cmd)
+{
+  //set bus to command/ctrl mode
+  MRT_PBUS_MODE(dev->mHandle, PBUS_MODE_CMD);
+
+  //write command byte
+  MRT_PBUS_WRITE(dev->mHandle, cmd);
 }
 
 mrt_status_t sed15xx_refresh(sed15xx_t* dev)
@@ -29,8 +56,14 @@ mrt_status_t sed15xx_refresh(sed15xx_t* dev)
   //disable display while we write the data
   sed15xx_enable(dev, false);
 
+  // make sure we are in data mode
+  MRT_PBUS_MODE(dev->mHandle, PBUS_MODE_DATA);
+
   //write data buffer
-  MRT_8080_DATA_WRITE(dev->mBuffer, dev->mBufferSize);
+  for(int i=0; i < dev->mBufferSize; i++)
+  {
+    MRT_PBUS_WRITE(dev->mHandle, dev->mBuffer[i]);
+  }
 
   //re-enable display
   sed15xx_enable(dev, true);
@@ -41,27 +74,27 @@ mrt_status_t sed15xx_refresh(sed15xx_t* dev)
 mrt_status_t sed15xx_enable(sed15xx_t* dev, bool val)
 {
   if(val)
-    MRT_8080_CMD(SED15XX_CMD_ENABLE(1));
-  else:
-    MRT_8080_CMD(SED15XX_CMD_ENABLE(0));
+    sed15xx_cmd( dev, SED15XX_CMD_DISPLAY_ON);
+  else
+    sed15xx_cmd( dev , SED15XX_CMD_DISPLAY_OFF);
 
   return MRT_STATUS_OK;
 }
 
 mrt_status_t sed15xx_sw_reset(sed15xx_t* dev)
 {
-  MRT_8080_CMD(SED15XX_CMD_RESET);
+  sed15xx_cmd( dev , SED15XX_CMD_RESET);
 
   return MRT_STATUS_OK;
 }
 
-mrt_status_t sed15xx_set_cursor(sed15xx_t* dev, uin16_t x, uin16_t y)
+mrt_status_t sed15xx_set_cursor(sed15xx_t* dev, uint16_t x, uint16_t y)
 {
   //TODO set cursor
   return MRT_STATUS_OK;
 }
 
-mrt_status_t sed15xx_pixel(sed15xx_t* dev,uin16_t x, uin16_t y, bool val )
+mrt_status_t sed15xx_pixel(sed15xx_t* dev,uint16_t x, uint16_t y, bool val )
 {
   //TODO
   return MRT_STATUS_OK;
@@ -73,14 +106,20 @@ mrt_status_t sed15xx_write_buffer(sed15xx_t* dev, uint8_t* data, int len)
   return MRT_STATUS_OK;
 }
 
-mrt_status_t sed15xx_draw_bmp(sed15xx_t* dev, uin16_t x, uin16_t y, GFXBmp bmp)
+mrt_status_t sed15xx_draw_bmp(sed15xx_t* dev, uint16_t x, uint16_t y, GFXBmp bmp)
 {
   //TODO
   return MRT_STATUS_OK;
 }
 
-mrt_status_t sed15xx_print( uin16_t x, uint16_t y, const char * text)
+mrt_status_t sed15xx_print( sed15xx_t* dev, uint16_t x, uint16_t y, const char * text)
 {
   //TODO
+  return MRT_STATUS_OK;
+}
+
+mrt_status_t sed15xx_fill(sed15xx_t* dev, uint8_t val)
+{
+  memset(dev->mBuffer, val, dev->mBufferSize);
   return MRT_STATUS_OK;
 }
--- sed15xx.h
+++ sed15xx.h
@@ -8,19 +8,35 @@
 
 #include "Platforms/Common/mrt_platform.h"
 #include "Utilities/GFX/Fonts/gfxfont.h"
+#include <string.h>
 
 
 
 //Macros for commands
-#define SED15XX_CMD_ENABLE(x) (0xAE | (x))
-#define SED15XX_CMD_START_LINE(x) (0xC0 | (x))
-#define SED15XX_CMD_PAGE_ADDR(x) (0xAC | (x))
-#define SED15XX_CMD_SET_COLUMN(x) (x)
-#define SED15XX_CMD_MODIFY 0xE0
-#define SED15XX_CMD_END 0xEE
-#define SED15XX_CMD_RESET 0xE2
+#define SED15XX_CMD_REGULATOR_RESISTOR_RATIO_SET(s)  (0x20 | (s))
+#define SED15XX_CMD_POWER_CONTROL(s)                 (0x28 | (s))
+#define SED15XX_CMD_CONTRAST_SET                     (0x81)
+#define SED15XX_CMD_ADC_SELECT_NORMAL                (0xA0)
+#define SED15XX_CMD_ADC_SELECT_REVERSE               (0xA1)
+#define SED15XX_CMD_LCD_BIAS_1_9                     (0xA2)
+#define SED15XX_CMD_LCD_BIAS_1_7                     (0xA3)
+#define SED15XX_CMD_DISPLAY_ALL_POINTS_OFF           (0xA4)
+#define SED15XX_CMD_DISPLAY_ALL_POINTS_ON            (0xA5)
+#define SED15XX_CMD_DISPLAY_NORMAL                   (0xA6)
+#define SED15XX_CMD_DISPLAY_REVERSE                  (0xA7)
+#define SED15XX_CMD_DISPLAY_OFF                      (0xAE)
+#define SED15XX_CMD_DISPLAY_ON                       (0xAF)
+#define SED15XX_CMD_COM_NORMAL                       (0xC0)
+#define SED15XX_CMD_COM_REVERSE                      (0xC8)
+#define SED15XX_CMD_READ_MODIFY_WRITE                (0xE0)
+#define SED15XX_CMD_END                              (0xEE)
+#define SED15XX_CMD_RESET                            (0xE2)
+#define SED15XX_CMD_BOOSTER_RATIO_SET                (0xF8)
+#define SED15XX_CMD_START_LINE_SET(line)             (0x40 | (line))
+#define SED15XX_CMD_PAGE_ADDRESS_SET(page)           (0xD0 | (page))
+#define SED15XX_CMD_COLUMN_ADDRESS_SET_HIGH(column)  (0x10 | ((column >> 4) & 0x0F))
+#define SED15XX_CMD_COLUMN_ADDRESS_SET_LOW(column)   (0x00 | ((column >> 0) & 0x0F))
 
-#define
 
 typedef struct{
   int mWidth;                   //width of display in pixels
@@ -29,7 +45,7 @@ typedef struct{
   GFXfont* mFont;               //font to use for printing
   uint8_t* mBuffer;             //buffer of pixel data
   int mBufferSize;
-  mrt_8080_handle_t mHandle;    //platform dependent handle for parallel bus
+  mrt_pbus_handle_t mHandle;    //platform dependent handle for parallel bus
   bool mInverted;
 }sed15xx_t;
 
@@ -42,7 +58,14 @@ typedef struct{
   *@param height height (in pixels) of display buffer
   *@return MRT_STATUS_OK
   */
-mrt_status_t sed15xx_init(sed15xx_t* dev, mrt_8080_handle_t handle,  int width, int height);
+mrt_status_t sed15xx_init(sed15xx_t* dev, mrt_pbus_handle_t handle,  int width, int height);
+
+/**
+  *@brief sends command over parallel bus to device
+  *@param dev ptr to sed15xx device
+  *@param cmd command to send
+  */
+void sed15xx_cmd(sed15xx_t* dev, uint8_t cmd);
 
 /**
   *@brief updates the device with the local buffer
@@ -73,7 +96,7 @@ mrt_status_t sed15xx_sw_reset(sed15xx_t* dev);
   *@param y y coordinate
   *@return status of operation
   */
-mrt_status_t sed15xx_set_cursor(sed15xx_t* dev, uin16_t x, uin16_t y);
+mrt_status_t sed15xx_set_cursor(sed15xx_t* dev, uint16_t x, uint16_t y);
 
 /**
   *@brief writes a single pixel in the buffer
@@ -83,7 +106,7 @@ mrt_status_t sed15xx_set_cursor(sed15xx_t* dev, uin16_t x, uin16_t y);
   *@param val val for pixel
   *@return status of operation
   */
-mrt_status_t sed15xx_pixel(sed15xx_t* dev,uin16_t x, uin16_t y, bool val );
+mrt_status_t sed15xx_pixel(sed15xx_t* dev,uint16_t x, uint16_t y, bool val );
 
 /**
   *@brief writes an array of bytes to the buffer
@@ -102,7 +125,7 @@ mrt_status_t sed15xx_write_buffer(sed15xx_t* dev, uint8_t* data, int len);
   *@param bmp bitmap to draw
   *@return status of operation
   */
-mrt_status_t sed15xx_draw_bmp(sed15xx_t* dev, uin16_t x, uin16_t y, GFXBmp bmp);
+mrt_status_t sed15xx_draw_bmp(sed15xx_t* dev, uint16_t x, uint16_t y, GFXBmp bmp);
 
 /**
   *@brief Draws rendered text to the buffer
@@ -112,4 +135,12 @@ mrt_status_t sed15xx_draw_bmp(sed15xx_t* dev, uin16_t x, uin16_t y, GFXBmp bmp);
   *@param text text to be written
   *@return status of operation
   */
-mrt_status_t sed15xx_print( uin16_t x, uint16_t y, const char * text);
+mrt_status_t sed15xx_print(sed15xx_t* dev, uint16_t x, uint16_t y, const char * text);
+
+/**
+  *@brief fill buffer with value
+  *@param dev ptr to device
+  *@param val value to write
+  *@return status of operation
+  */
+mrt_status_t sed15xx_fill(sed15xx_t* dev, uint8_t val);
